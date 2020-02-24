d9ec6f7019a4bd63d3ff046965f276058c9bc25f
Brian Salomon <bsalomon@akamai.com>
2020-02-24 13:11:16 -0500

    Update to 21.11.3

--- AkaMap.framework/Headers/VocSdkVersion.h
+++ AkaMap.framework/Headers/VocSdkVersion.h
@@ -6,5 +6,5 @@
 
 #define VOCSDK_MAJOR			21
 #define VOCSDK_MINOR			11
-#define VOCSDK_BUILD			2
-#define VOCSDK_VERSION_STRING	"21.11.2"
+#define VOCSDK_BUILD			3
+#define VOCSDK_VERSION_STRING	"21.11.3"
